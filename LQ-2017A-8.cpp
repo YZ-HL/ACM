6f49d219a7fd09ab558c2906fdb0bbd09c9f5f20
yz-hl <yz_hl@oi-liu.com>
2021-02-09 01:23:26 +0800

    ChickenMcnugget！

--- LQ-2017A-8.cpp
+++ LQ-2017A-8.cpp
@@ -1,15 +1,21 @@
-//骗分写法
-//首次提交88，调参后75，88，100
 #include <bits/stdc++.h>
 #define maxn 105
-#define maxm 500005
+#define maxm 10005
 using namespace std;
-int n, a[maxn], dp[maxm];
+int n, g, a[maxn], dp[maxm];
 int main(void)
 {
     scanf("%d", &n);
     for(int i = 1; i <= n; i++)
         scanf("%d", &a[i]);
+    g = a[1];
+    for(int i = 2; i <= n; i++)
+        g = __gcd(g, a[i]);
+    if(g != 1)
+    {
+        printf("INF\n");
+        return 0;
+    }
     dp[0] = 1;
     for(int i = 1; i <= n; i++)
         for(int j = a[i]; j <= maxm - 5; j++)
@@ -17,9 +23,6 @@ int main(void)
     int cnt = 0;
     for(int i = 1; i <= maxm - 5; i++)
         if(!dp[i])   cnt++;
-    if(cnt > 5000)
-        printf("INF\n");
-    else
-        printf("%d\n", cnt);
+    printf("%d\n", cnt);
     return 0;
 }
