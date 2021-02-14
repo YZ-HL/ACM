5195e75be1f9bd75b86c7b686ac5552c02ca23db
yz-hl <yz_hl@oi-liu.com>
2021-02-15 00:38:48 +0800

    LQ-2018A-10 AC "I am SB!!!"

--- LQ-2018A-10.cpp
+++ LQ-2018A-10.cpp
@@ -1,63 +1,28 @@
 #include <bits/stdc++.h>
 #define maxn 500005
+#define int long long
 using namespace std;
 int n, S, a[maxn];
-int flag1 = 1, flag2 = 1;
-namespace subtask1{
-    void solve(){
-        printf("0.0000\n");
-    }
-}
-namespace subtask2{
-    void solve(int now){
-        printf("%d\n", now);
-        double avg = 1.0 * S / n;
-        double sum1 = 0;
-        for(int i = 1; i <= n; i++)
-        {
-            if(a[i] == 0)
-                sum1 += avg * avg;
-            else
-                sum1 += ((1.0 * S / now) - avg) * ((1.0 * S / now) - avg);
-        }
-        printf("%.4lf\n", sqrt(sum1 / n));
-    }
-}
-int main(void)
+signed main(void)
 {
-    scanf("%d %d", &n, &S);
+    scanf("%lld %lld", &n, &S);
     for(int i = 1; i <= n; i++)
-        scanf("%d", &a[i]);
-    for(int i = 2; i <= n; i++)
-        if(a[i] != a[i - 1])
-        {
-            flag1 = 0;
-            break;
-        }
-    if(flag1)
-    {
-        subtask1::solve();
-        return 0;
-    }
+        scanf("%lld", &a[i]);
     sort(a + 1, a + n + 1);
+    double r = S, sum = 0;
     for(int i = 1; i <= n; i++)
-        if(a[i] != 0)
-        {
-            flag2 = i + 1;
-            break;
-        }
-    int num = n - flag2 + 2;
-    for(int i = flag2; i <= n; i++)
-        if(a[i] != a[i - 1])
-        {
-            flag2 = 0;
-            break;
-        }
-    if(flag2)
     {
-        subtask2::solve(num);
-        return 0;
+        if(a[i] < r / (n - i + 1))
+        {
+            sum += (a[i] - 1.0 * S / n) * (a[i] - 1.0 * S / n);
+            r -= a[i];
+        }
+        else
+        {
+            sum += (n - i + 1) * (r / (n - i + 1) - 1.0 * S / n) * (r / (n - i + 1) - 1.0 * S / n);
+            break;
+        } 
     }
-    printf("0.0001\n");
+    printf("%.4lf", sqrt(sum / n));
     return 0;
 }
