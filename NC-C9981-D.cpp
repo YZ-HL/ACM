c540bce0cbfe662bbe1f275dc373b0cd88f27fb8
yz-hl <yz_hl@oi-liu.com>
2021-02-22 14:26:21 +0800

    重编号冲突导致的BUG Fix!

--- NC-C9981-D.cpp
+++ NC-C9981-D.cpp
@@ -1,77 +1,78 @@
 #include <bits/stdc++.h>
-#define maxn 505
-#define mod 1000000007
+#define maxn 1005
 #define int long long
+#define mod 1000000007
 using namespace std;
-int n, k, tx, ty, uset[maxn * maxn], num[maxn * maxn];
+int n, k, tx, ty;
+int cnt, siz[maxn * maxn], uset[maxn * maxn];
 char board[maxn][maxn];
-char get_read(){
+char get_char(){
     char ch = getchar();
     while(ch != '1' && ch != '0')    ch = getchar();
     return ch;
 }
+int calc(int x, int y){
+    return x * n + y;
+}
 int find(int x){
-    return x == uset[x] ? uset[x] : uset[x] = find(uset[x]);
+    return x == uset[x] ? x : uset[x] = find(uset[x]);
 }
 void unioon(int x, int y){
     int fx = find(x), fy = find(y);
     if(fx == fy)    return;
     uset[fx] = fy;
-    num[fy] += num[fx];
+    siz[fy] += siz[fx];
 }
 int quickpow(int a, int b, int p){
-    if(b == 0)  return 1;
+    if(b == 0)    return 1;
     int tmp = quickpow(a, b / 2, p);
     tmp = (tmp * tmp) % p;
-    if(b & 1)   tmp = (tmp * a) % p;
+    if(b & 1)    tmp = (tmp * a) % p;
     return tmp;
 }
 int inv(int x){
-    int p = mod;
-    return quickpow(x, p - 2, p);
+    return quickpow(x, mod - 2, mod);
 }
+int cnum, vis[maxn][maxn];
 int fx[] = {0, 1, -1, 0, 0};
 int fy[] = {0, 0, 0, 1, -1};
+bool dfs(int x, int y){
+    if(vis[x][y])    return false;
+    if(board[x][y] == '0')    return false;
+    if(x < 1 || y < 1 || x > n || y > n)    return false;
+    
+    cnum++; vis[x][y] = 1;
+    for(int i = 1; i <= 4; i++)
+    {
+        int nx = x + fx[i], ny = y + fy[i];
+        if(dfs(nx, ny))
+            unioon(calc(x, y), calc(nx, ny));
+    }
+    return true;
+}
 signed main(void)
 {
     scanf("%lld", &n);
     for(int i = 1; i <= n; i++)
-    {
         for(int j = 1; j <= n; j++)
         {
-            board[i][j] = get_read();
-            uset[i * (n - 1) + j] = i * (n - 1) + j;
-            num[i * (n - 1) + j] = 1;
+            board[i][j] = get_char();
+            uset[calc(i, j)] = calc(i, j);
         }
-    }
-
+    int ans = 1;
     for(int i = 1; i <= n; i++)
-    {
         for(int j = 1; j <= n; j++)
         {
-            for(int k = 1; k <= 4; k++)
+            if(!vis[i][j] && board[i][j] == '1')
             {
-                int nx = i + fx[k], ny = j + fy[k];
-                if(board[nx][ny] == '1' && board[i][j] == '1')
-                {
-                    unioon(i * (n - 1) + j, nx * (n - 1) + ny);
-                }
-            }
-        }
-    }
-
-    int cnt = 0, ans = 1;
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= n; j++)
-            if(i * (n - 1) + j == uset[i * (n - 1) + j] && board[i][j] == '1')
-            {
+                cnum = 0;
+                dfs(i, j);
+                siz[find(calc(i, j))] = cnum;
+                ans = (ans * siz[find(calc(i, j))]) % mod;
                 cnt++;
-                ans = (ans * num[i * (n - 1) + j]) % mod;
-                //printf("debug: %lld %lld %lld\n", i, j, num[i * (n - 1) + j]);
             }
-
+        }    
     for(int i = 1; i <= cnt; i++)    ans = (ans * i) % mod;
-
     scanf("%lld", &k);
     for(int i = 1; i <= k; i++)
     {
@@ -82,30 +83,27 @@ signed main(void)
             printf("%lld\n", ans);
             continue;
         }
-        board[tx][ty] = '1';
         cnt++;
+        board[tx][ty] = '1';
+        siz[calc(tx, ty)] = 1;
         ans = (ans * cnt) % mod;
-        for(int i = 1; i <= 4; i++)
+        for(int k = 1; k <= 4; k++)
         {
-            int nx = tx + fx[i], ny = ty + fy[i];
-            if(board[nx][ny] == '0')    continue;
-            if(nx < 1 || ny < 1 || nx > n || ny > n)    continue;
-            int f1 = find(tx * (n - 1) + ty);
-            int f2 = find(nx * (n - 1) + ny);
-            //printf("%lld %lld %lld\n", i, nx, ny);
-            if(f1 == f2)  continue;
+            int nx = tx + fx[k];
+            int ny = ty + fy[k];
+            if(board[nx][ny] != '1')    continue;
+            int f1 = find(calc(tx, ty));
+            int f2 = find(calc(nx, ny));
+            if(f1 == f2)    continue;
             ans = (ans * inv(cnt)) % mod;
-            //printf("debug1:%lld %lld\n", cnt, inv(cnt));
-            ans = (ans * inv(num[f1])) % mod;
-            //printf("debug2:%lld\n", ans);
-            ans = (ans * inv(num[f2])) % mod;
-            //printf("debug3:%lld\n", ans);
-            ans = (ans * (num[f1] + num[f2])) % mod;
+            ans = (ans * inv(siz[f1])) % mod;
+            ans = (ans * inv(siz[f2])) % mod;
+            ans = (ans * (siz[f1] + siz[f2])) % mod;
+            //printf("%lld %lld %lld %lld\n", siz[f1], siz[f2], cnt, inv(cnt));
             cnt--;
-            //printf("%lld %lld %lld %lld %lld %lld\n", f1, num[f1], f2, num[f2], ans, cnt);
             unioon(f1, f2);
         }
-        printf("%lld\n", ans % mod);
+        printf("%lld\n", ans);
     }
     return 0;
 }
