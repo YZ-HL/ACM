f4dcd1d7d87d7392b65955dec5a9a674a8b4ce1b
yz-hl <yz_hl@oi-liu.com>
2021-05-24 12:44:07 +0800

    20210524

--- CF715-Div2-C.cpp
+++ CF715-Div2-C.cpp
@@ -1,38 +1,20 @@
 #include <bits/stdc++.h>
-#define maxn 500005
+#define maxn 2005
 #define int long long
 using namespace std;
-int n, m, s[maxn], now[maxn];
-const int INF = (1LL << 60);
-int ans = INF;
-void show(){
-    printf("show debug:\n");
-    for(int i = 1; i <= n; i++)
-        printf(i == n ? "%d\n" : "%d ", now[i]);
+int n, m, s[maxn], dp[maxn][maxn];
+int dfs(int lf, int rt){
+    if(lf == rt)     return 0;
+    if(~dp[lf][rt])  return dp[lf][rt];
+    return dp[lf][rt] = s[rt] - s[lf] + min(dfs(lf + 1, rt), dfs(lf, rt - 1));
 }
 signed main(void)
 {
+    memset(dp, -1, sizeof(dp));
     scanf("%lld", &n);
     for(int i = 1; i <= n; i++)
         scanf("%lld", &s[i]);
     sort(s + 1, s + n + 1);
-    while(m <= n)
-    {
-        for(int i = n; i >= n - m + 1; i--)
-            now[i] = s[n - i + 1];
-        for(int i = 1; i <= n - m; i++)
-            now[i] = s[i + m];
-        //show();
-        int maxx = -INF, minn = INF, sum = 0;
-        for(int i = 1; i <= n; i++)
-        {
-            maxx = max(maxx, now[i]);
-            minn = min(minn, now[i]);
-            sum += (maxx - minn);
-        }
-        ans = min(ans, sum);
-        m++;
-    }
-    printf("%lld\n", ans);
+    printf("%lld\n", dfs(1, n));
     return 0;
 }
